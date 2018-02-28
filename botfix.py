d49db4db7bbb489e113c552c35abea4cc43566a8
JonasKs <jonas-ks@hotmail.com>
2018-02-28 19:20:59 +0100

    Rebuilt structure
    ..to support reasons without quotations

--- botfix.py
+++ botfix.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 import sqlite3
 
-description = '''A bot to update the website database.'''
+description = '''A bot to update the banlog site.'''
 bot = commands.Bot(command_prefix='!', description=description)
 
 @bot.event
@@ -13,46 +13,48 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.event
-async def on_message(message):
-    if message.channel.id == str('channel1ID') or message.channel.id == str('channel2ID'):
-        await bot.process_commands(message)
-#@bot.command()
-#async def help(name="help"):
-#    """Commands for this bot. Ask Hotfix#6638. """
-#    await bot.say('You can use this bot to update the reason for bans and unbans on the website. Example: !update ban hotfix "Banned because of.."')
-
 @bot.command()
-async def updategame(game : str):
-    """Update what I'm playing, just for fun."""
+async def botupdategame(game : str):
+    """Update what I'm playing"""
     await bot.change_presence(game=discord.Game(name=game))
 
-@bot.group(pass_context=True)
-async def update(ctx):
-    """Example: !update <command> <name> "<Reason>" """
-    if ctx.invoked_subcommand is None:
-        await bot.say('You need to add a reason')
-
-@update.command()
-async def ban(user: str, reason: str):
+@bot.event
+async def on_message(message):
     """Ban command, editing reason in database. NO FAULTHANDLING IMPLEMENTED AT THIS POINT"""
-    sqlite_file = '/home/ubuntu/twitch-bans/db.sql'
-    await bot.say('updating ban for {}: {}'.format(user, reason))
-    conn = sqlite3.connect(sqlite_file)
-    c = conn.cursor()
-    c.execute("UPDATE bans SET reason=? WHERE username=? AND type='ban'", (reason, user))
-    conn.commit()
-    conn.close()
+    #Check that the message is sent to a mod channel:
+    if message.channel.id == str(CHANNELID) or message.channel.id == str(CHANNELID):
+        if message.content.startswith('!update ban'):
+            content = message.content
+            crop = content[12:]
+            string = crop.split(' ', 1)
+            username = string[0]
+            reason = string[1]
+            melding = 'Updating ban for `{}` with reason `{}`'.format(username,reason)
+            await bot.send_message(message.channel, melding)
 
-@update.command()
-async def unban(user: str, reason: str):
-    """Unban command, editing reason in database. NO FAULTHANDLING IMPLEMENTED AT THIS POINT"""
-    sqlite_file = '/home/ubuntu/twitch-bans/db.sql'
-    await bot.say('updating ban for {}: {}'.format(user, reason))
-    conn = sqlite3.connect(sqlite_file)
-    c = conn.cursor()
-    c.execute("UPDATE bans SET reason=? WHERE username=? AND type='unban'", (reason, user))
-    conn.commit()
-    conn.close()
 
+            sqlite_file = '/home/ubuntu/twitch-bans/db.sql'
+            conn = sqlite3.connect(sqlite_file)
+            c = conn.cursor()
+            c.execute("UPDATE bans SET reason=? WHERE username=? AND type='ban'", (reason, username))
+            conn.commit()
+            conn.close()
+
+        elif message.content.startswith('!update unban'):
+            """Unban command, editing reason in database. NO FAULTHANDLING IMPLEMENTED AT THIS POINT"""
+            content = message.content
+            crop = content[14:]
+            string = crop.split(' ', 1)
+            username = string[0]
+            reason = string[1]
+            melding = 'Updating unban for `{}` with reason `{}`'.format(username,reason)
+            await bot.send_message(message.channel, melding)
+
+
+            sqlite_file = '/home/ubuntu/twitch-bans/db.sql'
+            conn = sqlite3.connect(sqlite_file)
+            c = conn.cursor()
+            c.execute("UPDATE bans SET reason=? WHERE username=? AND type='unban'", (reason, username))
+            conn.commit()
+            conn.close()
 bot.run('DiscordBotToken')
